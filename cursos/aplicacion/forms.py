46192b19c3e7b37ea6ec2640caf2fdde415883c5
NormanBeltran <josenormanbeltran@gmail.com>
2024-03-12 20:39:18 -0300

    Clase 23: Autenticacion, Login, Logout, Registracion

---
+++ cursos/aplicacion/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class CursoForm(forms.Form):
+    nombre = forms.CharField(max_length=50, required=True)
+    comision = forms.IntegerField(required=True)
+
+class ProfesorForm(forms.Form):
+    nombre = forms.CharField(max_length=60, required=True)
+    apellido = forms.CharField(max_length=60, required=True)
+    email = forms.EmailField()
+    profesion = forms.CharField(max_length=50, required=True)    
+
+class RegistroForm(UserCreationForm): 
+    email = forms.EmailField(required=True)   
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirma Contraseña", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
